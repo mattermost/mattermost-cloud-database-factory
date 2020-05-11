5e7206ed2cbfe75181b41725e2b9dc683342bc9e
Stylianos Rigas <stylianosrigas@gmail.com>
2020-05-11 15:56:33 +0300

    MM-24686 Running Terraform fmt for terraform files

--- terraform/aws/database-factory/variables.tf
+++ terraform/aws/database-factory/variables.tf
@@ -1,153 +1,153 @@
 variable "vpc_id" {
-    default = ""
-    type = string
+  default = ""
+  type    = string
 }
 
 variable "db_id" {
-    default = ""
-    type = string
+  default = ""
+  type    = string
 }
 
 variable "environment" {
-    default = ""
-    type = string
+  default = ""
+  type    = string
 }
 
 variable "port" {
-    default = "3306"
-    type = string
+  default = "3306"
+  type    = string
 }
 
 variable "engine" {
-    default = "aurora-mysql"
-    type = string
+  default = "aurora-mysql"
+  type    = string
 }
 
 variable "engine_version" {
-    default = "5.7.12"
-    type = string
+  default = "5.7.12"
+  type    = string
 }
 
 variable "username" {
-    default = "mmcloud"
-    type = string
+  default = "mmcloud"
+  type    = string
 }
 
 variable "password" {
-    default = ""
-    type = string
-    description = "If empty a random password will be created for each RDS Cluster and stored in AWS Secret Management."
+  default     = ""
+  type        = string
+  description = "If empty a random password will be created for each RDS Cluster and stored in AWS Secret Management."
 }
 
 variable "final_snapshot_identifier_prefix" {
-    default = "final"
-    type = string
+  default = "final"
+  type    = string
 }
 
 variable "skip_final_snapshot" {
-    default = false
-    type = bool
+  default = false
+  type    = bool
 }
 
 variable "deletion_protection" {
-    default = true
-    type = bool
+  default = true
+  type    = bool
 }
 
 variable "backup_retention_period" {
-    default = ""
-    type = string
+  default = ""
+  type    = string
 }
 
 variable "preferred_backup_window" {
-    default = "02:00-03:00"
-    type = string
+  default = "02:00-03:00"
+  type    = string
 }
 
 variable "preferred_maintenance_window" {
-    default = "sun:05:00-sun:06:00"
-    type = string
+  default = "sun:05:00-sun:06:00"
+  type    = string
 }
 
 variable "storage_encrypted" {
-    default = true
-    type = bool
+  default = true
+  type    = bool
 }
 
 variable "apply_immediately" {
-    default = true
-    type = bool
+  default = true
+  type    = bool
 }
 
 variable "copy_tags_to_snapshot" {
-    default = true
-    type = bool
+  default = true
+  type    = bool
 }
 
 variable "enabled_cloudwatch_logs_exports" {
-    default = ["audit", "error", "general", "slowquery"]
-    type = list(string)
+  default = ["audit", "error", "general", "slowquery"]
+  type    = list(string)
 }
 
 variable "instance_type" {
-    default = ""
-    type = string
+  default = ""
+  type    = string
 }
 
 variable "monitoring_interval" {
-    default = 60
-    type = number
+  default = 60
+  type    = number
 }
 
 variable "performance_insights_enabled" {
-    default = false
-    type = bool
+  default = false
+  type    = bool
 }
 
 variable "replica_scale_max" {
-    default = 15
-    type = number
-    description = "Maximum number of replicas to scale up to."
+  default     = 15
+  type        = number
+  description = "Maximum number of replicas to scale up to."
 }
 
 variable "replica_scale_min" {
-    default = 1
-    type = number
+  default = 1
+  type    = number
 }
 
 variable "replica_min" {
-    default = 2
-    type = number
-    description = "Number of replicas to deploy initially with the RDS Cluster."
+  default     = 2
+  type        = number
+  description = "Number of replicas to deploy initially with the RDS Cluster."
 }
 
 variable "predefined_metric_type" {
-    default = "RDSReaderAverageCPUUtilization"
-    type = string
+  default = "RDSReaderAverageCPUUtilization"
+  type    = string
 }
 
 variable "replica_scale_cpu" {
-    default = 70
-    type = number
-    description = "Needs to be set when predefined_metric_type is RDSReaderAverageCPUUtilization"
+  default     = 70
+  type        = number
+  description = "Needs to be set when predefined_metric_type is RDSReaderAverageCPUUtilization"
 }
 
 variable "replica_scale_connections" {
-    default = ""
-    type = string
-    description = "Needs to be set when predefined_metric_type is RDSReaderAverageDatabaseConnections"
+  default     = ""
+  type        = string
+  description = "Needs to be set when predefined_metric_type is RDSReaderAverageDatabaseConnections"
 }
 
 variable "replica_scale_in_cooldown" {
-    default = 300
-    type = number
-    description = "Cooldown in seconds before allowing further scaling operations after a scale in"
+  default     = 300
+  type        = number
+  description = "Cooldown in seconds before allowing further scaling operations after a scale in"
 }
 
 variable "replica_scale_out_cooldown" {
-    default = 300
-    type = number
-    description = "Cooldown in seconds before allowing further scaling operations after a scale out"
+  default     = 300
+  type        = number
+  description = "Cooldown in seconds before allowing further scaling operations after a scale out"
 }
 
 

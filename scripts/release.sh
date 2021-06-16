460021f3e76c61a0b47bade9caeedc3090e178e8
Stavros Foteinopoulos <stafot@gmail.com>
2021-06-16 18:48:56 +0300

    Fix release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -15,7 +15,7 @@ fi
 LATEST_RELEASE_SHA=$(hub rev-parse "${LATEST_RELEASE}")
 LATEST_RELEASE_NEXT_COMMIT_SHA=$(hub log "${LATEST_RELEASE}"..HEAD --oneline --pretty=%H | tail -n1)
 mkdir -p ./build/_output/docs/
-release-notes --org mattermost --repo mattermost-cloud-database-factory --start-sha "${LATEST_RELEASE_NEXT_COMMIT_SHA}" --end-sha "${FUTURE_RELEASE_SHA}"  --output ./build/_output/docs/relnote.md --required-author "" --branch main
+release-notes --org mattermost --repo mattermost-cloud-database-factory --start-sha "${LATEST_RELEASE_NEXT_COMMIT_SHA}" --end-sha "${FUTURE_RELEASE_SHA}"  --output ./build/_output/docs/relnote.md --required-author "" --branch master
 cat ./build/_output/docs/relnote.md | sed '/docs.k8s.io/ d' | sed -e "s/\# Release notes for/${CIRCLE_TAG}/g" | sed -e "s/Changelog since/Changelog since ${LATEST_RELEASE}/g"> ./build/_output/docs/relnote_parsed.md
 echo "\n" >> ./build/_output/docs/relnote_parsed.md
 echo "The image for this release is \`docker.io/mattermost/mattermost-cloud-database-factory:${CIRCLE_TAG}\`" >> ./build/_output/docs/relnote_parsed.md
